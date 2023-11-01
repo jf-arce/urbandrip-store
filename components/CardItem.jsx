652b816905eac424416571fd594a8ea4e7d93498
jf-arce <josefranciscoarce21@gmail.com>
2023-10-31 22:49:23 -0300

    last update

--- components/CardItem.jsx
+++ components/CardItem.jsx
@@ -16,9 +16,9 @@ export const CardItem = ({prod}) => {
   return (
     <div className="shadow-md overflow-hidden rounded-b-md" onMouseEnter={handlOnMouseEnter} onMouseLeave={handlOnMouseLeave}>
         <Link href={`/product/${prod.id}`} >
-            <picture>
+            <div className="h-[300px] overflow-hidden">
                 <img src={prod.image} alt={prod.title} className={`h-[300px] w-full object-cover ${ onHover && "scale-105"} transition-transform`} />
-            </picture>
+            </div>
             <div className="p-4 flex flex-col items-center">
                 <h3 className="h-14 text-center">{prod.title}</h3>
                 <p className="font-bold">${prod.price}</p>
